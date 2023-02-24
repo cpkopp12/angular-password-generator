7fd0b56f97965d6fbf165ce0975eed866484dbfb
cpkopp <cameronpkopp@gmail.com>
2023-02-23 22:13:16 -1000

    AppCompenent class properties and methods handle user input

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  //properties
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  //event handlers
+  onChangeLength($event: any) {
+    const inputVal = $event.target.value;
+    const parsedValue = parseInt(inputVal);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+  onChangeLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  //generate button handler
+  onButtonClick() {
+    console.log(`
+    length: ${this.length}
+    letters: ${this.includeLetters}
+    numbers: ${this.includeNumbers}
+    symbols: ${this.includeSymbols}
+    `);
+    this.password = 'MY PASSWORD'
+  }
 }
